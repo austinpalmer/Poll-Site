c9d0410d287dbf6451423cc083132e887f1ba679
austinpalmer <austinpalmer178@gmail.com>
2024-06-08 13:08:33 -0500

    Setup PostGRE database connection, created the DB models, and setup the Django site.

--- polls/models.py
+++ polls/models.py
@@ -10,11 +10,11 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
-
+    # Find if the question was published within the past day
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-# Model for chocie (response) to a question
+# Model for choice (response) to a question
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
